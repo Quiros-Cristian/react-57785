dc3231b0269d0c0b8c7e36c0e2fe39148752c6b0
Quiros Cristian <quiroscristian12@gmail.com>
2024-06-25 09:33:08 -0300

    responsive al sitio

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,42 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import category from "../data/categorias.json"
+import Logo from "../assets/img/Luxila.png"
+import CartWidget from './header/CartWidget'
 
 const NavBar = () => {
 
 
   return (
-      <nav className='nav'>
-        <ul className='navmenu'>
-          <li>
-            <NavLink to="/" activeclassname="active" className='nav-link '>Inicio</NavLink>
-          </li>
-          <li>
-            <NavLink to="/productos" activeclassname="active" className='nav-link'>Productos</NavLink>
-          </li>
-          {
-            category.map((category)=>{
-              return(
-              <li key={category.id}>
-                <NavLink to={`/category/${category.id}`} activeclassname="active" className='nav-link'>{category.nombre}</NavLink>
+
+      <nav class="navbar navbar-expand-lg bg-body-tertiary nav">
+        <div class="container-fluid">
+        <NavLink to={'/'} className={'link-texto'} ><h1 className="logoTexto"><img className="logo" src={Logo} />LucilaTejidos</h1></NavLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav navmenu">
+              <li>
+                <NavLink to="/" activeclassname="active" className='nav-link '>Inicio</NavLink>
               </li>
-              )
-            })
-          }
-        </ul>
-      </nav>
+              <li>
+                <NavLink to="/productos" activeclassname="active" className='nav-link'>Productos</NavLink>
+              </li>
+              {
+                category.map((category) => {
+                  return (
+                    <li key={category.id}>
+                      <NavLink to={`/category/${category.id}`} activeclassname="active" className='nav-link'>{category.nombre}</NavLink>
+                    </li>
+                  )
+                })
+              }
+            <CartWidget />
+            </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import NavBar from "../NavBar";
 import CartWidget from "./CartWidget";
-import { NavLink } from "react-router-dom";
-import Logo from "../../assets/img/Luxila.png";
 
 const Header = () => {
 
   return (
     <header className="header">
-      <NavLink to={'/'} className={'link-texto'} ><h1 className="logoTexto"><img className="logo" src={Logo} />LucilaTejidos</h1></NavLink>
       <NavBar />
-      <CartWidget />
     </header>
   );
 };
